b67786fe38d46d72da51576a50a7c7f40c0410c0
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-09 17:38:37 +0100

    add SynsetType

---
+++ lib/type/synset.dart
@@ -0,0 +1,18 @@
+part of "main.dart";
+
+// NOTE: only type, EnvironmentType child
+class SynsetType {
+  final int id;
+  final String name;
+  final String shortname;
+  SynsetType({required this.id, required this.name, required this.shortname});
+
+  factory SynsetType.fromJSON(Map<String, dynamic> o) {
+    return SynsetType(
+        id: o["id"] as int, name: o["name"] as String, shortname: (o["shortname"] ?? '') as String);
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {"id": id, "name": name, "shortname": shortname};
+  }
+}
