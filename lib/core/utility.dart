94725cb2df13e4bd7a84b94e9ddda7b23ca3e417
Khen Solomon Lethil <khensolomon@gmail.com>
2022-01-09 17:43:44 +0100

    clean

--- lib/core/utility.dart
+++ lib/core/utility.dart
@@ -1,15 +1,3 @@
 part of 'main.dart';
 
-mixin _Utility {
-  final UtilAnalytics utilAnalytics = new UtilAnalytics();
-
-  Future<void> analyticsSearch(String searchTerm) async {
-    if (searchTerm.isNotEmpty) await utilAnalytics.search(searchTerm);
-  }
-
-  Future<void> analyticsScreen(String name, String classes) async {
-    // await new FirebaseAnalytics().setCurrentScreen(creenName: 'home',screenClassOverride: 'HomeState');
-    // debugPrint('analyticsScreen $name $classes');
-    await utilAnalytics.send.setCurrentScreen(screenName: name,screenClassOverride: classes);
-  }
-}
+mixin _Utility {}
