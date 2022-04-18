f5f31a1906eae31623a70efdc0d78853fe56ae22
Khen Solomon Lethil <khensolomon@gmail.com>
2022-04-18 20:29:52 +0200

    signInWithApple

--- lib/core/authentication.dart
+++ lib/core/authentication.dart
@@ -2,4 +2,9 @@ part of data.core;
 
 class Authentication extends UnitAuthentication {
   // Authentication({name, options}) : super(name: name, options: options);
+  Authentication()
+      : super(
+          appleServiceId: 'com.myordbok.app.signin',
+          redirectUri: 'https://myordbok-app.firebaseapp.com/__/auth/handler',
+        );
 }
